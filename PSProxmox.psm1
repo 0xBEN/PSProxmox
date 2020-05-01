a981b08c14cb0ced4da27167db1cb047e158dd31
bit1ngest0r <benheater@gmail.com>
2020-05-01 00:35:32 -0400

    First commit

---
+++ PSProxmox.psm1
@@ -0,0 +1,44 @@
+# Author: Benjamin Heater
+
+#region Import Scripts
+$publicFunctions = Get-ChildItem -Path "$PSScriptRoot\Public\ps1"
+$privateFunctions = Get-ChildItem -Path "$PSScriptRoot\Private\ps1"
+$publicFunctions | ForEach-Object { . $_.FullName }
+$privateFunctions | ForEach-Object { . $_.FullName }
+#endregion Import Scripts
+
+#region Export Public Functions and Aliases
+$aliases = @()
+$publicFunctions | ForEach-Object { # Export all of the public functions from this module
+    
+    $hasAlias = Get-Content $_.FullName | Select-String "\[alias" # Check if any of the functions have aliases set eg. [alias("shortcut", "scut")] by reading the text of the script file
+    if ($hasAlias) { # If alias string found (see example above)
+
+        $filter = $hasAlias.Line.Split('"*",') -replace "\[\w+\(", "" -replace "\)\]", "" # Split the string where there is a pattern of '"string",' -- creating an array -- then replace the [alias()] characters with whitespace
+        $alias = $filter | Where-Object { -not[string]::IsNullOrWhiteSpace($_) } # Remove any whitespace from the string, leaving only characters
+        $aliases += $alias
+
+        Export-ModuleMember -Function $_.BaseName -Alias $alias
+
+    }
+    else {
+
+        Export-ModuleMember -Function $_.BaseName
+
+    }
+
+}
+#endregion Export Public Functions and Aliases
+
+if (-not $aliases) { $aliases = "*" }
+try {
+
+    Update-ModuleManifest -Path "$PSScriptRoot\PSProxmox.psd1" -FunctionsToExport $publicFunctions.BaseName -AliasesToExport $aliases
+
+}
+catch {
+
+    Out-Null
+
+
+}
